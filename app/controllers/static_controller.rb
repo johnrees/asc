b9b8f6f8342f4139adfe70307fd242be16d626f8
johnrees <john@bitsushi.com>
2013-02-23 20:17:24 +0100

    lots of additions

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -7,4 +7,9 @@ class StaticController < ApplicationController
 
   def terms
   end
+
+  def management_team
+    @management_team = %w(chris_hooley nick_filce tony_stilwell richard_jowett)
+    @sales_team = %w(mark_kelsey lilya_lecoq nadya_gooders alex_hooley)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,23 @@
 class Post < ActiveRecord::Base
   attr_accessible :content, :image, :published_at, :title
+
+  def self.featured
+    limit(4)
+  end
+
+  def self.latest
+    offset(4).limit(6)
+  end
+
+  def self.archive
+    offset(10)
+  end
+
   def self.homepage
   	limit(3)
   end
 
   def to_s
     title
-end
+  end
 end
