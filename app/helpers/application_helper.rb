88b8fd64b2fc9122a4cd351fdb9d29ead1da9ca6
johnrees <john@bitsushi.com>
2013-02-25 14:25:31 +0100

    temp fix for images on heroku

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,9 @@ module ApplicationHelper
 
   def title page_title, image = nil, heading = nil
     content_for(:title) { page_title.to_s }
-    content_for(:hero_bg) { image || image_path('header_images/' + action_name + '.jpg') }
+    if image.present?
+      content_for(:hero_bg) { image || image_path('header_images/' + action_name + '.jpg') }
+    end
     content_for(:heading) { heading || page_title.to_s }
   end
 
