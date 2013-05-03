ddd192289f321a12ff85005d4dc101f66c162da0
johnrees <john@bitsushi.com>
2013-05-03 16:50:33 +0200

    final amends

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -9,7 +9,8 @@ class StaticController < ApplicationController
   end
 
   def management_team
-    @management_team = %w(chris_hooley nick_filce tony_stilwell richard_jowett ellie_goldsmith karen_jenner ben_pinington)
-    @sales_team = %w(mark_kelsey lilya_lecoq nadya_gooders alex_hooley)
+    @management_team = %w(chris_hooley nick_filce tony_stilwell mark_kelsey)
+    @management_team2 = %w( richard_jowett ellie_goldsmith karen_jenner ben_pinington)
+    @sales_team = %w( alex_hooley nadya_gooders lilya_lecoq  )
   end
 end
