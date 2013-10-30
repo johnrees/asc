0073e6c128a46c86b07208e75bb63558de170e5c
John Rees <john@bitsushi.com>
2013-10-30 13:25:51 +0000

    cloned github

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -11,6 +11,6 @@ class StaticController < ApplicationController
   def management_team
     @management_team = %w(chris_hooley nick_filce tony_stilwell mark_kelsey)
     @management_team2 = %w( richard_jowett ellie_goldsmith karen_jenner ben_pinington)
-    @sales_team = %w( alex_hooley nadya_gooders lilya_lecoq  )
+    @sales_team = %w( alex_hooley nadya_gooders  )
   end
 end
